87208bec5e483937680a8856217f097759c3ed8b
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-24 13:30:43 +0200

    Span in Header component modified

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Inventory extends React.Component {
 	render() {
 		return (
-		<p className="lolo">Heyoo</p>
+		<div className="lolo">Heyoo</div>
 	)}
 }
 
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Order extends React.Component {
 	render() {
 		return (
-			<p className="lolo">Heyy</p>
+			<div className="lolo">Heyy</div>
 	)}
 }
 
