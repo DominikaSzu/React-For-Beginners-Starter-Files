56c397afcb6d0b29a56f3805cd1e4c309c46f0de
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-31 11:50:57 +0100

    Adding button to elements

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,14 +3,16 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
 	render() {
-		const { image, name, status, price } = this.props.details;
+		const { image, name, status, price, desc } = this.props.details;
 
 		return (
 			<li className="menu-fish">
 				<img src={image} alt={name} />
-				<h3 className="fish-name">{name}
+				<h4 className="fish-name">{name}
 					<span className="price">{formatPrice(price)}</span>
-				</h3>
+				</h4>
+				<p>{desc}</p>
+				<button>Add To Cart</button>
 			</li>
 	)}
 }
