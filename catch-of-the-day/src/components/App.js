c5c14efb9df509ed4983e97360b69087fc9858fc
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-14 11:54:28 +0100

    Formatting the price in Order component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -39,7 +39,7 @@ render() {
 			})}
 			</ul>
 		</div>
-		<Order />
+		<Order fishes={this.state.fishes} order={this.state.order} />
 		<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
 	</div>				
 	)
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { formatPrice } from '../helpers.js';
 
 class Order extends React.Component {
 	render() {
+		const orderId = Object.keys(this.props.order);
+		const total = orderId.reduce((prevTotal, key) => {
+			const fish = this.props.fishes[key];
+			const count = this.props.order[key];
+			const isAvailable = fish && fish.status === 'available';
+
+			if (isAvailable) {
+				return prevTotal + (count * fish.price);
+			} 
+
+			return prevTotal;
+		}, 0);
+
+
+
 		return (
 			<div className="order-wrap">
 				<h2>Order</h2>
+				{orderId}
+				<div className="total">
+				<strong>{formatPrice(total)}</strong>
+				</div>
 			</div>
 	)}
 }
